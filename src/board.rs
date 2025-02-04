670b945e3784ebb50ad937e0fe4a314cf2e97e7b
Barfussmann <Simon.Thiesen@online.de>
2025-02-04 22:48:43 +0100

    fully working

--- src/board.rs
+++ src/board.rs
@@ -1,33 +1,10 @@
-use crate::Ship;
+use crate::ship::Ship;
 use crate::{BOARD_SIZE, SHIPS, SIZE};
 
-static PLACED_BITS_SHIPS: [[[[BitBoard; SIZE]; SIZE]; 2]; 10] = {
-    let mut placed_ships = [[[[BitBoard::new(Board::new()); SIZE]; SIZE]; 2]; 10];
+use std::fmt::Display;
+use std::fmt::Write;
 
-    let mut ship_i = 0;
-    while ship_i < 10 {
-        let mut dir_i = 0;
-        while dir_i < 2 {
-            let mut y = 0;
-            while y < SIZE {
-                let mut x = 0;
-                while x < SIZE {
-                    placed_ships[ship_i][dir_i][y][x] =
-                        BitBoard::new(PLACED_SHIPS[ship_i][dir_i][y][x]);
-
-                    x += 1;
-                }
-                y += 1;
-            }
-            dir_i += 1;
-        }
-        ship_i += 1;
-    }
-
-    placed_ships
-};
-
-static PLACED_SHIPS: [[[[Board; SIZE]; SIZE]; 2]; 10] = {
+pub static PLACED_SHIPS: [[[[Board; SIZE]; SIZE]; 2]; 10] = {
     let mut placed_ships = [[[[Board::new(); SIZE]; SIZE]; 2]; 10];
 
     let mut ship_i = 0;
@@ -60,11 +37,6 @@ static PLACED_SHIPS: [[[[Board; SIZE]; SIZE]; 2]; 10] = {
     placed_ships
 };
 
-use std::fmt::Display;
-use std::fmt::Write;
-use std::mem::transmute;
-use std::simd::u64x2;
-
 #[derive(Debug, Clone, Copy)]
 pub enum Direction {
     Horizontal = 0,
@@ -89,184 +61,6 @@ impl Display for Cell {
         };
         f.write_str(str)
     }
-}
-
-#[derive(Debug)]
-pub enum WasShipplacmentSuccsessfull {
-    Yes,
-    No,
-}
-
-#[derive(Clone, Copy)]
-#[repr(align(32))]
-pub struct BitBoard {
-    pub protected: u64x2,
-    pub ship: u64x2,
-}
-impl BitBoard {
-    pub const fn new(board: Board) -> Self {
-        let mut protected = 0u128;
-        let mut ship = 0u128;
-
-        let mut i = 0;
-
-        while i < u128::BITS as usize {
-            let bit_index = Self::map_index_to_bit_index(i);
-            match board.cells[i] {
-                Cell::Water => {}
-                Cell::Protected => protected |= 1 << bit_index,
-                Cell::Ship | Cell::ShipHit => {
-                    ship |= 1 << bit_index;
-                    protected |= 1 << bit_index
-                }
-            }
-
-            i += 1;
-        }
-        Self {
-            protected: unsafe { transmute::<u128, u64x2>(protected) },
-            ship: unsafe { transmute::<u128, u64x2>(ship) },
-        }
-    }
-    const fn map_index_to_bit_index(index: usize) -> usize {
-        if index < 40 {
-            index
-        } else {
-            (index - 40) + 64 // put it in to the next u64 to make further calculations easier
-        }
-    }
-    // #[inline(never)]
-    fn place_ship(&mut self, index: usize, ship: Ship) {
-        let placed_ship_board = unsafe {
-            PLACED_BITS_SHIPS
-                .get_unchecked(ship.index)
-                .as_flattened()
-                .as_flattened()
-                .get_unchecked(index)
-        };
-
-        self.protected |= placed_ship_board.protected;
-        self.ship |= placed_ship_board.ship;
-    }
-    // #[inline(never)]
-    fn allowable_ship_placements(&self, ship: Ship) -> (u64x2, u64x2) {
-        const Y_SHIP_MASK: [u64x2; 8] = {
-            let mut masks = [u64x2::from_array([0; 2]); 8];
-
-            let mut ship_length = 1;
-            while ship_length < masks.len() {
-                let low = (1 << (4 * SIZE)) - 1; // group of the lower 4 rows
-                let high = (1 << ((7 - ship_length) * SIZE)) - 1; // when the ship length is over 1 the top rows are cut off
-
-                masks[ship_length] = u64x2::from_array([low, high]);
-                ship_length += 1;
-            }
-
-            masks
-        };
-        const X_SHIP_MASK: [u64x2; 8] = {
-            let mut masks = [u64x2::from_array([0; 2]); 8];
-
-            let mut ship_length = 1;
-            while ship_length < masks.len() {
-                let mut mask = 0u128;
-
-                let single_row_allowable = (1 << (SIZE - (ship_length - 1))) - 1;
-
-                let mut y = 0;
-                while y < SIZE {
-                    let bit_index = BitBoard::map_index_to_bit_index(y * SIZE);
-
-                    mask |= single_row_allowable << bit_index;
-                    y += 1
-                }
-
-                masks[ship_length] = unsafe { transmute::<u128, u64x2>(mask) };
-                ship_length += 1;
-            }
-
-            masks
-        };
-
-        let allowable = !self.protected;
-
-        let mut ship_placements_x = allowable;
-        for i in 1..ship.length() as u64 {
-            ship_placements_x &= allowable >> i;
-        }
-        ship_placements_x &= X_SHIP_MASK[ship.length()];
-
-        let mut ship_placements_y = allowable;
-        for i in 1..ship.length() {
-            let high = allowable[1];
-            let high_shifted_by_one = high >> SIZE;
-
-            let low_shifted_by_one = (allowable[0] >> SIZE) & ((1 << 40) - 1);
-            let low_with_high = low_shifted_by_one | (high << 30);
-
-            ship_placements_y &=
-                u64x2::from_array([low_with_high, high_shifted_by_one]) >> ((i - 1) * SIZE) as u64;
-        }
-        ship_placements_y &= Y_SHIP_MASK[ship.length()];
-
-        (ship_placements_x, ship_placements_y)
-    }
-    #[inline(never)]
-    pub fn random_place_ship(&mut self, ship: Ship, random_value: u32, rng: &mut fastrand::Rng) {
-        let (ship_placements_x, ship_placements_y) = self.allowable_ship_placements(ship);
-        let x_ships = ship_placements_x[0].count_ones() + ship_placements_x[1].count_ones();
-        let y_ships = ship_placements_y[0].count_ones() + ship_placements_y[1].count_ones();
-
-        let total_index = random_value % (x_ships + y_ships);
-        // let total_index = rng.u8(..(x_ships + y_ships) as u8) as u32;
-
-        let (bit_map, index, offset) = if total_index < x_ships {
-            (ship_placements_x, total_index, 0)
-        } else {
-            (ship_placements_y, total_index - x_ships, SIZE * SIZE) // there is a brache for ship_placments because ther are 128 and not 64 but and thus can't be move by cmov
-        };
-        let index = set_bit_to_index(bit_map, index) as usize + offset;
-
-        // let index = if total_index < x_ships {
-        //     set_bit_to_index(ship_placements_x, total_index) as usize
-        // } else {
-        //     // 0
-        //     (SIZE * SIZE) + set_bit_to_index(ship_placements_y, total_index - x_ships) as usize
-        // };
-
-        self.place_ship(index, ship)
-    }
-}
-
-fn nth_set_bit_index_u64(num: u64, n: u32) -> u32 {
-    let spread_bits = unsafe { std::arch::x86_64::_pdep_u64(1 << n, num) };
-    spread_bits.trailing_zeros()
-}
-// #[inline(never)]
-fn set_bit_to_index(set_bits: u64x2, valid_ship_index: u32) -> u32 {
-    // num[0].count_ones() + num[1].count_ones()
-
-    let low_set_bits = set_bits[0].count_ones();
-
-    let (target_n, num, offset) = if valid_ship_index < low_set_bits {
-        (valid_ship_index, set_bits[0], 0)
-    } else {
-        (valid_ship_index - low_set_bits, set_bits[1], 40)
-    };
-    nth_set_bit_index_u64(num, target_n) + offset
-
-    // 'outer: {
-    //     let mut count = 0;
-    //     for i in 0..128 {
-    //         if num & (1 << i) != 0 {
-    //             if count == n {
-    //                 break 'outer i;
-    //             }
-    //             count += 1;
-    //         }
-    //     }
-    //     0
-    // };
 }
 
 #[derive(Debug, Clone, Copy)]
--- src/main.rs
+++ src/main.rs
@@ -1,157 +1,36 @@
 #![feature(portable_simd)]
 #![allow(dead_code)]
-use std::fmt::{Display, Write};
+
 use std::{iter::zip, time::Instant};
 mod board;
 
+use bit_board::BitBoard;
+use board::Board;
 use board::Cell;
-use board::{BitBoard, Board, set_protecet_at_offsets};
 use num_format::{Locale, ToFormattedString};
-use rand::Rng;
-use rand::thread_rng;
+use rand::{Rng, thread_rng};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use rayon::slice::ParallelSlice;
 
 const SIZE: usize = 10;
 const BOARD_SIZE: usize = (SIZE * SIZE).next_multiple_of(64);
 
-const SHIPS: &[Ship] = &[
-    Ship::new(4, 0),
-    Ship::new(3, 1),
-    Ship::new(3, 2),
-    Ship::new(2, 3),
-    Ship::new(2, 4),
-    Ship::new(2, 5),
-    Ship::new(1, 6),
-    Ship::new(1, 7),
-    Ship::new(1, 8),
-    Ship::new(1, 9),
+const SHIPS: &[ship::Ship] = &[
+    ship::Ship::new(4, 0),
+    ship::Ship::new(3, 1),
+    ship::Ship::new(3, 2),
+    ship::Ship::new(2, 3),
+    ship::Ship::new(2, 4),
+    ship::Ship::new(2, 5),
+    ship::Ship::new(1, 6),
+    ship::Ship::new(1, 7),
+    ship::Ship::new(1, 8),
+    ship::Ship::new(1, 9),
 ];
-// const SHIPS: &[Ship] = &[
-//     // Ship::new(4, 0),
-//     Ship::new(4, 1),
-//     // Ship::new(1, 2),
-//     // Ship::new(1, 3),
-//     // Ship::new(1, 4),
-//     // Ship::new(1, 5),
-//     // Ship::new(1, 6),
-//     // Ship::new(1, 7),
-//     // Ship::new(1, 8),
-//     // Ship::new(1, 9),
-// ];
 
-#[derive(Debug, Clone)]
-struct ShipCounts {
-    counts: [u64; BOARD_SIZE],
-    board_count: u64,
-}
-
-impl ShipCounts {
-    fn new() -> ShipCounts {
-        ShipCounts {
-            counts: [0; BOARD_SIZE],
-            board_count: 0,
-        }
-    }
-    fn add_board(&mut self, board: Board) {
-        for (count, cell) in zip(&mut self.counts, &board.cells) {
-            match cell {
-                Cell::Ship => {
-                    *count += 1;
-                }
-                Cell::Protected | Cell::Water | Cell::ShipHit => {}
-            }
-        }
-        self.board_count += 1;
-    }
-    #[inline(never)]
-    fn add_bit_board(&mut self, board: BitBoard) {
-        for i in 0..40 {
-            self.counts[i] += ((board.ship[0] & (1 << i)) != 0) as u64;
-        }
-        // high bits
-        for i in 0..60 {
-            self.counts[i + 40] += ((board.ship[1] & (1 << i)) != 0) as u64;
-        }
-
-        self.board_count += 1;
-    }
-    fn add_other_count(&mut self, other: Self) {
-        for (self_count, other_count) in zip(&mut self.counts, &other.counts) {
-            *self_count += *other_count;
-        }
-        self.board_count += other.board_count;
-    }
-}
-impl Display for ShipCounts {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_char('\n')?;
-        for row in self.counts.chunks(SIZE).take(SIZE) {
-            for count in row {
-                let probability = (*count as f64) / (self.board_count as f64);
-
-                f.write_fmt(format_args!("{:4.3} ", probability))?;
-            }
-            f.write_char('\n')?;
-        }
-        Ok(())
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum ShipLength {
-    _1 = 1,
-    _2 = 2,
-    _3 = 3,
-    _4 = 4,
-    _5 = 5,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Ship {
-    length: ShipLength,
-    index: usize,
-}
-
-impl Ship {
-    const fn new(length: usize, index: usize) -> Ship {
-        let length = match length {
-            1 => ShipLength::_1,
-            2 => ShipLength::_2,
-            3 => ShipLength::_3,
-            4 => ShipLength::_4,
-            5 => unreachable!(), // implement y shift by 5 in allowable_ship_placements. Currently it doesn't work because it would have to get a second amount data from high
-            // 5 => ShipLength::_5,
-            _ => unreachable!(),
-        };
-        Ship { length, index }
-    }
-    const fn length(&self) -> usize {
-        self.length as usize
-    }
-}
-pub struct BadRNG(u64);
-impl BadRNG {
-    fn new() -> Self {
-        let val: u64 = thread_rng().r#gen();
-        Self(val)
-    }
-    #[inline]
-    pub fn gen_u64(&mut self) -> u64 {
-        // Constants for WyRand taken from: https://github.com/wangyi-fudan/wyhash/blob/master/wyhash.h#L151
-        // Updated for the final v4.2 implementation with improved constants for better entropy output.
-        const WY_CONST_0: u64 = 0x2d35_8dcc_aa6c_78a5;
-        const WY_CONST_1: u64 = 0x8bb8_4b93_962e_acc9;
-
-        let s = self.0.wrapping_add(WY_CONST_0);
-        self.0 = s;
-        let t = u128::from(s) * u128::from(s ^ WY_CONST_1);
-        (t as u64) ^ (t >> 64) as u64
-    }
-}
-
-const DIAGONAL_OFFSETS: [(i32, i32); 4] = [(-1, -1), (1, -1), (-1, 1), (1, 1)];
-const ORTHOGONAL_OFFSETS: [(i32, i32); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+mod bit_board;
+mod ship;
+mod ship_counts;
 
 #[allow(unused_mut)]
 fn main() {
@@ -160,60 +39,51 @@ fn main() {
     //     .build_global()
     //     .unwrap();
 
-    let iterations = 25_000_000u64;
-    let random_values: Box<[u32]> = (0..iterations * SHIPS.len() as u64)
-        .into_par_iter()
-        .map(|_| rand::thread_rng().r#gen())
-        .collect();
+    let iterations = 400_000_000u64;
+    // let iterations = 140_000_000u64;
 
     let mut start_board = Board::new();
     let mut start_ships = SHIPS.to_vec();
     loop {
-        let start_time = Instant::now();
-        // let iterations = 10_000_000u64;
+        let random_values: Box<[u32]> = (0..iterations * SHIPS.len() as u64)
+            .into_par_iter()
+            .map(|_| rand::thread_rng().r#gen())
+            .collect();
 
         let bit_board = BitBoard::new(start_board);
 
         let mut board = bit_board;
-        let rng = &mut fastrand::Rng::new();
         for ship in &start_ships {
-            board.random_place_ship(*ship, rng.u32(..), rng);
+            board.random_place_ship(*ship, thread_rng().gen_range(0..u32::MAX));
         }
+        let start_time = Instant::now();
 
         let ship_counts = random_values[..]
             .par_chunks(start_ships.len())
             // let ship_counts = (0..iterations)
             //     .into_par_iter()
             .fold(
-                || (ShipCounts::new(), fastrand::Rng::new()),
-                |(mut ship_counts, mut rng), rand_values| {
-                    step(
-                        &start_ships,
-                        bit_board,
-                        &mut ship_counts,
-                        rand_values,
-                        &mut rng,
-                    );
+                ship_counts::ShipCounts::new,
+                |mut ship_counts, rand_values| {
+                    step(&start_ships, bit_board, &mut ship_counts, rand_values);
                     #[inline(never)]
                     fn step(
-                        start_ships: &[Ship],
+                        start_ships: &[ship::Ship],
                         bit_board: BitBoard,
-                        ship_counts: &mut ShipCounts,
+                        ship_counts: &mut ship_counts::ShipCounts,
                         rand_values: &[u32],
-                        rng: &mut fastrand::Rng,
                     ) {
                         let mut board = bit_board;
 
                         for (ship, random_value) in zip(start_ships, rand_values) {
-                            board.random_place_ship(*ship, *random_value, rng);
+                            board.random_place_ship(*ship, *random_value);
                         }
 
                         ship_counts.add_bit_board(board);
                     }
-                    (ship_counts, rng)
+                    ship_counts
                 },
             )
-            .map(|(ship_counts, _)| ship_counts)
             .collect::<Vec<_>>()
             .into_iter()
             .reduce(|mut a, b| {
@@ -301,3 +171,6 @@ fn main() {
         // println!("Board: {}", start_board);
     }
 }
+
+const DIAGONAL_OFFSETS: [(i32, i32); 4] = [(-1, -1), (1, -1), (-1, 1), (1, 1)];
+const ORTHOGONAL_OFFSETS: [(i32, i32); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
